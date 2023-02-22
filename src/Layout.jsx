6452f269c2dd733d829302490cd37d6520357eef
Justin Suh <justindjsuh@gmail.com>
2023-02-22 16:21:38 -0500

    added logo files and layout for navigation

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
+const Layout = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
