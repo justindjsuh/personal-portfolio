1920d343beed94335f5d832a8894fd21d050dd93
Justin Suh <justindjsuh@gmail.com>
2023-02-24 02:08:35 -0500

    base jsx setup for about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import "./About.css";
 
 const About = () => {
   useEffect(() => {
@@ -7,7 +8,30 @@ const About = () => {
       page.classList.remove("animate_content");
     }, 2000);
   });
-  return <h1>ABOUT PAGE</h1>;
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </div>
+      <div className="toolkit-container">
+        <p>Toolkit</p>
+        <div className="toolkit">
+          <div className="tool-section frontend"></div>
+          <div className="tool-section backend"></div>
+          <div className="tool-section tools"></div>
+          <div className="tool-section learning"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
